cf108dfe0369f6c29878f5040d586a19552dd4e1
AustinFnFordham <47194352+AustinFnFordham@users.noreply.github.com>
2019-12-16 10:24:06 -0500

    Attributes
    ***Character Sheet Related***
    The attributes determine certain skill checks and the playerCharacters overall stats.
    Not really sure how to set this up in terms of linking it to the Character.

---
+++ Attributes.py
@@ -0,0 +1,30 @@
+#The attributes determine certain skill checks and the playerCharacters overall stats.
+
+import random
+
+class Attribute:
+    def __init__(self, default=8):
+       self.dex = dexterity
+       self.cha = charisma
+       self.stg = strength
+       self.con = constitution
+       self.int = intelligence
+       self.wis = wisdom
+
+    def dex(self): 
+        pass
+
+    def cha(self): 
+        pass
+
+    def stg(self): 
+        pass
+
+    def con(self):
+        pass
+
+    def int(self):
+        pass
+
+    def wis(self):
+        pass
