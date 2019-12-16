91f83c7168e5bd809f491881870b20e75b99a3e0
AustinFnFordham <47194352+AustinFnFordham@users.noreply.github.com>
2019-12-16 11:23:48 -0500

    Classes
    General classes determine base skills, powers, starter weapons and armor, and what archetypes are eventually available. 
    
    Maybe each class should be its own 'class' object, but I'm not sure how this works yet.

---
+++ Classes.py
@@ -0,0 +1,28 @@
+# General classes determine base skills, powers, starter weapons and armor, and what archetypes are eventually available. 
+
+class Classes:
+    def __init__(self):
+        self.barbarian = barbarian
+        self.rogue = rogue
+        self.soldier = soldier
+        self.ranger = ranger
+        self.wizard = wizard
+        self.warlock = warlock
+
+    def barb(self):
+        pass
+
+    def rogue(self):
+        pass
+
+    def soldier(self):
+        pass
+
+    def ranger(self):
+        pass
+
+    def wiz(self):
+        pass
+
+    def war(self):
+        pass
