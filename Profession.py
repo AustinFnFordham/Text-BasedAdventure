8e8872c34f5fb65113e6cc8befe8571b4fcbf261
AustinFnFordham <47194352+AustinFnFordham@users.noreply.github.com>
2019-12-16 10:41:27 -0500

    Professions
    ***PlayerCharacter and Skills***
    Professions would add a skill to the playerCharacter "Skill List" or a bonus to an attribute.

---
+++ Profession.py
@@ -0,0 +1,6 @@
+#Professions would add a skill to the playerCharacter "Skill List" or a bonus to an attribute.
+
+list_of_professions = ['Acolyte', 'Charlatan', 'Criminal', 
+                      'Entertainer', 'Folk Hero', 'Guild Artisan', 
+                       'Hermit', 'Outlander', 'Noble', 'Sage', 'Sailor',
+                       'Soldier', 'Urchin']
