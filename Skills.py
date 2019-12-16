ca9f9d16307f8c348653024f112e8722251add0d
AustinFnFordham <47194352+AustinFnFordham@users.noreply.github.com>
2019-12-16 11:18:15 -0500

    Skills
    Skills effect the  proficiency of certain actions within the game and affect the diceRolls.
     ex: "You are trying to get past the guard. What do you do? X(Bluff[Cha +2]), Y, Z" 
    giving options to use your skills within the game

---
+++ Skills.py
@@ -0,0 +1,11 @@
+# Skills effect the  proficiency of certain actions within the game and affect the diceRolls.
+# ex: "You are trying to get past the guard. What do you do? X(Bluff[Cha +2]), Y, Z" 
+# giving options to use your skills within the game
+
+skills_list = ['acrobatics', 'athletics', 'bluff',
+               'culture', 'diplomacy',
+               'disguise', 'engineering', 'intimidate',
+               'life_science', 'medicine', 'mysticism',
+               'perception', 'physical_science',
+               'sense_motive', 'sleight_of_hand',
+               'stealth', 'survival']
